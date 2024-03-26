6442ea6c3f55bc6d27c60f227b39a5d85fcd7767
l-gomes-ar <lgaraujo021@gmail.com>
2024-03-26 21:13:49 +0000

    Links dynamically built in the course homepage

--- scripts/links.js
+++ scripts/links.js
@@ -1,2 +1,27 @@
 const baseURL = 'https://l-gomes-ar.github.io/wdd230/';
-const linksURL = 'https://l-gomes-ar.github.io/wdd230/data/links.json'
+const linksURL = 'https://l-gomes-ar.github.io/wdd230/data/links.json';
+
+async function getLinks(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayLinks(data.weeks);
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        const li = document.createElement('li');
+        let content = `${week.week}: `;
+
+        week.links.forEach((link) => {
+            content += `<a href="${link.url}">${link.title}</a> | `;
+        });
+        
+        // Remove any remaining "|" character from the content string 
+        content = content.slice(0, content.length - 2);
+
+        li.innerHTML = content;
+        document.querySelector('ul').appendChild(li);
+    });
+};
+
+getLinks(linksURL);
