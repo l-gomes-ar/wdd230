e53dc7981cf848f68bfe666ced146a377f5d6799
l-gomes-ar <lgaraujo021@gmail.com>
2024-03-26 19:36:22 +0000

    Weather info added to course home page

--- scripts/getInformationDetails.js
+++ scripts/getInformationDetails.js
@@ -3,3 +3,34 @@ numberVisits++;
 localStorage.setItem('numberVisits', numberVisits);
 
 document.querySelector('#numberVisits').textContent = numberVisits;
+
+const weatherInfo = document.querySelector('#weatherInfo');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=51.55&lon=-0.31&units=metric&appid=ae52103434cc485984e651ca57270156';
+
+async function apiFetch(url) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+function displayResults(data) {
+    const para = document.createElement('p');
+    para.textContent = `${data.main.temp}°C - ${data.weather[0].main}`;
+
+    const weatherIcon = document.createElement('img');
+    weatherIcon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    weatherIcon.setAttribute('alt', 'Weather icon information for Wembley, GB');
+
+    weatherInfo.appendChild(weatherIcon);
+    weatherInfo.appendChild(para);
+}
+
+apiFetch(url);
---
+++ scripts/links.js
@@ -0,0 +1,2 @@
+const baseURL = 'https://l-gomes-ar.github.io/wdd230/';
+const linksURL = 'https://l-gomes-ar.github.io/wdd230/data/links.json'
