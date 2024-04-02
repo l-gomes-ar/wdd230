3008c8f83fc1c7e58ac24e8ec6f60aa11bcb7de6
l-gomes-ar <lgaraujo021@gmail.com>
2024-04-02 02:46:43 +0100

    Adding enhancements for the chamber home page pt2

---
+++ chamber/scripts/spotlight.js
@@ -0,0 +1,47 @@
+const memberSpotlight = document.querySelector('#member-spotlight');
+const url = 'https://l-gomes-ar.github.io/wdd230/chamber/data/members.json';
+
+
+async function fetchApi(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayData(data.companies);
+}
+
+function displayData(companies) {
+    const companiesSilverGold = companies.filter((company) => company.membershipLevel === 'Silver' || company.membershipLevel === 'Gold');
+    console.log(companiesSilverGold);
+
+    let randomList = [];
+
+    const choose = (arr) => arr[Math.floor(arr.length * Math.random())];
+
+    let company;
+    for (let i = 0; i < 3; i++) {
+        company = choose(companiesSilverGold);
+    
+        const tryPush = (company) => {
+            if (!randomList.includes(company)) {
+                randomList.push(company);
+            } else {
+                company = choose(companiesSilverGold);
+                tryPush(company);
+            }
+        };
+
+        tryPush(company);
+    }
+
+    randomList.forEach((company) => {
+        memberSpotlight.innerHTML += `<div>
+                                         <h3>${company.name}</h3>
+                                         <p class="description">${company.description}</p>
+                                         <p class="address">${company.address}</p>
+                                         <p class="phone">${company.phone}</p>
+                                     </div>`;
+        
+        
+    });
+}
+
+fetchApi(url);
